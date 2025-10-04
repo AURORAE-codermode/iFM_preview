18d2b169647b3906cb2f4e9b0e1693ee3b2bdd81
AURORAE-codermode <fitzroy20230909@outlook.com>
2025-10-04 20:50:04 +0800

    Create AppConstants.kt

---
+++ AppConstants.kt
@@ -0,0 +1,18 @@
+package com.zjgsu.ifm_preview.core.constants
+
+object AppConstants {
+    object Preferences {
+        const val SHARED_PREFS_NAME = "podcast_app_prefs"
+        const val KEY_USER_LOGGED_IN = "user_logged_in"
+        const val KEY_USER_NAME = "user_name"
+        const val KEY_USER_EMAIL = "user_email"
+    }
+    
+    object Player {
+        const val ACTION_PLAY = "PLAY"
+        const val ACTION_PAUSE = "PAUSE"
+        const val ACTION_STOP = "STOP"
+        const val ACTION_NEXT = "NEXT"
+        const val ACTION_PREVIOUS = "PREVIOUS"
+    }
+}
